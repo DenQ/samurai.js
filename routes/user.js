db8507c6ed599cc33bd7464664975633803938da
DenQ <denquick@gmail.com>
2015-12-23 22:28:55 +0300

    Rest describe

--- routes/user.js
+++ routes/user.js
@@ -5,21 +5,40 @@ var Samurai = require('../lib/samurai');
 
 class User extends Samurai.Route{
 
-    constructor() {
-        super();
-        //@howto: extract base class, and split constructor on REST methods
+    Get() {
         server.route({
             method: 'GET',
-            path: '/user/{name}',
+            path: '/user/{id}',
             handler: function (request, reply) {
-                reply('User name - , ' + encodeURIComponent(request.params.name) + '!');
+                reply('Get id - , ' + encodeURIComponent(request.params.id) + '!');
             }
         });
+    }
 
-
+    Post() {
         server.route({
-            method: 'GET',
-            path: '/user/id{id}',
+            method: 'POST',
+            path: '/user/{id}',
+            handler: function (request, reply) {
+                reply('User id - , ' + encodeURIComponent(request.params.id) + '!');
+            }
+        })
+    }
+
+    Put() {
+        server.route({
+            method: 'PUT',
+            path: '/user/{id}',
+            handler: function (request, reply) {
+                reply('User id - , ' + encodeURIComponent(request.params.id) + '!');
+            }
+        })
+    }
+
+    Delete() {
+        server.route({
+            method: 'DELETE',
+            path: '/user/{id}',
             handler: function (request, reply) {
                 reply('User id - , ' + encodeURIComponent(request.params.id) + '!');
             }
