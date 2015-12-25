cf8a6346a299235c4a70649b21de090a09287fdc
DenQ <denquick@gmail.com>
2015-12-25 21:22:44 +0300

    Colors. Controllers

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -1,12 +1,29 @@
 'use strict';
 var Samurai = require('../../lib/samurai');
+var colors = require('colors');
 
 class User extends Samurai.Controller{
 
     constructor() {
         super();
-        console.log(333);
     }
+
+    Get() {
+        console.log('controller get'.yellow);
+    }
+    GetAll() {
+        console.log('controller get all'.yellow);
+    }
+    Post() {
+        console.log('controller post'.yellow);
+    }
+    Put() {
+        console.log('controller put'.yellow);
+    }
+    Delete() {
+        console.log('controller delete'.yellow);
+    }
+
 }
 
 module.exports = User;
--- app/routes/user.js
+++ app/routes/user.js
@@ -3,23 +3,32 @@ var Samurai = require('../../lib/samurai');
 
 class User extends Samurai.Route{
 
+    constructor() {
+        super();
+    }
+
     Get() {
+        this.controllerAction();
         this.reply('Get id - , ' + encodeURIComponent(this.request.params.id) + '!');
     }
 
     GetAll() {
+        this.controllerAction();
         this.reply('Get all');
     }
 
     Post() {
+        this.controllerAction();
         this.reply('Get id - , ' + encodeURIComponent(this.request.params.id) + '!');
     }
 
     Put() {
+        this.controllerAction();
         this.reply('Get id - , ' + encodeURIComponent(this.request.params.id) + '!');
     }
 
     Delete() {
+        this.controllerAction();
         this.reply('Get id - , ' + encodeURIComponent(this.request.params.id) + '!');
     }
 }
