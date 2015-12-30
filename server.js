367187983b4a978397bfc6ddc37d0bfbe4018b5d
DenQ <denquick@gmail.com>
2015-12-30 20:50:39 +0300

    Config

--- server.js
+++ server.js
@@ -3,8 +3,11 @@
 const Hapi = require('hapi');
 const server = new Hapi.Server();
 const Good = require('good');
+const Config = require('./app/configs/main');
 
-server.connection({port: 5200});
+server.connection({
+    port: Config.port
+});
 
 server.register({
     register: Good,
